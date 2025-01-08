d8ec2c37d891a6691c33b1be858e55c31c5d7097
Adam_Babatunde_Sanusi <tundey520@gmail.com>
2025-01-09 00:29:24 +0100

    Swagger and uri versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  const config = new DocumentBuilder()
+    .setTitle('Nextlevity API')
+    .setDescription('The Nextlevity API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('nextlevity')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
